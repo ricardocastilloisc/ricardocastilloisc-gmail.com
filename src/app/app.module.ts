9dd4a2e3ff3315daa6fa492a44d3eeb9b8bad9c3
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2020-11-06 00:32:10 -0500

    6. Conectar el formulario de registro con una instancia del modelo de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { LoginComponent } from './pages/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,8 @@
+import { ThrowStmt } from '@angular/compiler';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'ricaror@hotmail.com'
+   }
 
   ngOnInit() { }
 
+  onSubmit()
+  {
+    console.log(this.usuario);
+  }
 
 }
